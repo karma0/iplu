27626f90592a897147abd4e74621c73d18b795e5
Bobby Larson <karma0@gmail.com>
2016-03-16 16:15:45 -0600

    Fixed a bug in the server preventing updates

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var Creds = require('./creds.json');
 var ips_file = "./ips.json";
 var IPs = require(ips_file);
 
-var getCreds = function (id, cb) { console.log(Creds[id]); return cb(null, Creds[id]); };
+var getCreds = function (id, cb) { return cb(null, Creds[id]); };
 
 var updateIP = function (user, ip, cb) {
   IPs[user] = ip;
@@ -25,30 +25,39 @@ var handler = function (req, res) {
     if (err) {
       respCode = 401;
       payload = 'Not Authorized';
+      respond(respCode, res, payload, headers, creds, artifacts);
     } else {
 
       if ((req.method === 'PUT') || (req.method === 'POST')) {
-        updateIP(creds.user, req.connection.remoteAddress, function(err) {
-          payload = !err ? 'OK' : "Could not update address.";
-        });
+        return updateIP(creds.user, req.connection.remoteAddress,
+          function(err) {
+            if (err)
+              console.log(err);
+            payload = (err ? "Could not update address." : 'OK');
+            respond(respCode, res, payload, headers, creds, artifacts);
+          });
       } else if (req.method === 'GET') {
         payload = IPs[creds.user];
+        return respond(respCode, res, payload, headers, creds, artifacts);
       }
+      respond(respCode, res, payload, headers, creds, artifacts);
 
     }
-
-    var header = Hawk.server.header(
-                      creds,
-                      artifacts,
-                      {
-                        payload: payload,
-                        contentType: headers['Content-Type']
-                      });
-    headers['Server-Authorization'] = header;
-
-    res.writeHead(respCode, headers);
-    res.end(payload);
   });
+};
+
+var respond = function(respCode, res, payload, headers, creds, artifacts) {
+  var header = Hawk.server.header(
+                    creds,
+                    artifacts,
+                    {
+                      payload: payload,
+                      contentType: headers['Content-Type']
+                    });
+  headers['Server-Authorization'] = header;
+
+  res.writeHead(respCode, headers);
+  res.end(payload);
 };
 
 var port = process.env.NODE_PORT ? process.env.NODE_PORT : 8000;
